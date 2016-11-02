434b5dd54895cddce9b2bb69002e111a13114331
defintuition <ahspadafora@gmail.com>
2016-11-02 16:34:28 -0400

    test

---
+++ NYT/NYT-Critics/NYT/NYT/Critics.swift
@@ -0,0 +1,18 @@
+//
+//  Critics.swift
+//  NYT
+//
+//  Created by Amber Spadafora on 11/2/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import Foundation
+
+class Critics {
+
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
