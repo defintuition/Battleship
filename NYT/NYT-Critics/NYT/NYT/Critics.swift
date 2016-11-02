fab8000834ba524eea09e880b48d3719754c0b24
defintuition <ahspadafora@gmail.com>
2016-11-02 16:44:25 -0400

    files

--- NYT/NYT-Critics/NYT/NYT/Critics.swift
+++ NYT/NYT-Critics/NYT/NYT/Critics.swift
@@ -8,11 +8,70 @@
 
 import Foundation
 
-class Critics {
+internal enum UserModelParseError: Error {
+    case results(json: Any)
+    case name(json: AnyObject)
+}
+
+class Critic {
 
     var name: String
 
     init(name: String) {
         self.name = name
+    }
+    
+    static func createCriticList(from data: Data) -> [Critic]? {
+        
+        var criticsList: [Critic]? = []
+        
+        do {
+            let jsonData: Any = try JSONSerialization.jsonObject(with: data, options: [])
+            
+            guard let response: [String: AnyObject] = jsonData as? [String: AnyObject],
+                let results: [AnyObject] = response["results"] as? [AnyObject] else {
+                    throw UserModelParseError.results(json: jsonData)
+            }
+            
+            for critic in results {
+                guard let name: String = critic["display_name"] as? String
+                    else {
+                        throw UserModelParseError.name(json: critic)
+                }
+                
+                let validCritic: Critic = Critic(name: name)
+                criticsList?.append(validCritic)
+            }
+            return criticsList
+            
+        }
+        
+        catch let UserModelParseError.results(json: json) {
+            print("Error encountered with parsing 'results' key for object \(json)")
+        }
+//
+        catch let UserModelParseError.name(json: json) {
+            print("Error encountered with parsing 'name' key for object \(json)")
+        }
+//
+//        catch let UserModelParseError.location(json: json) {
+//            print("Error encountered with parsing 'location' key for object \(json)")
+//        }
+//            
+//        catch let UserModelParseError.login(json: json) {
+//            print("Error encountered with parsing 'login' key for object \(json)")
+//        }
+//            
+//        catch let UserModelParseError.picture(json: json) {
+//            print("Error encountered with parsing 'login' key for object \(json)")
+//        }
+//            
+        catch {
+            print("Error encountered with parsing: \(error)")
+        }
+
+        print("returning nil")
+        return nil
+        
     }
 }
